70aadda2a004404d9e8a68d2bae6c74370b68c51
j-ack-bennett <bennettjack44@gmail.com>
2021-05-17 13:17:04 +1200

    imported components to show using route to

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+import AboutMe from './AboutMe'
+import Contact from './Contact'
+import Home from './Home'
+import Nav from './Nav'
+import Projects from './Projects'
+import Resume from './Resume'
+import Skills from './Skills'
+import '../styles/App.css'
+import '../styles/styles.css'
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Route path='/' exact component={Home} />
+      <Route to='/' exact component={AboutMe} />
+      <Route to='/' exact component={Skills} />
+      <Route to='/' exact component={Resume} />
+      <Route to='/' exact component={Projects} />
+      <Route to='/' exact component={Contact} />
+    </>
+  )
+}
+
+export default App
