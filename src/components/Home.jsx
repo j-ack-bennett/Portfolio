e5639bea3abde9980f537999db90b3696e17957e
j-ack-bennett <bennettjack44@gmail.com>
2021-05-17 13:17:40 +1200

    created component for home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section class="hero is-link is-fullheight is-fullheight-with-navbar">
+      <div class="hero-body">
+        <div class="container">Kia ora! I am
+          <h1 class="title is-1">
+            Jack Bennett
+          </h1>
+          <h2 class="subtitle is-3">
+            Full Stack Software Developer
+          </h2>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Home
