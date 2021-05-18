8158bc51b1e5dc88d60055f7654d0c5b10452baa
j-ack-bennett <bennettjack44@gmail.com>
2021-05-18 19:04:32 +1200

    added footer and commented out stylesheet

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
+import React from "react"
+import { Route } from "react-router-dom"
 
-import AboutMe from './AboutMe'
-import Contact from './Contact'
-import Home from './Home'
-import Nav from './Nav'
-import Projects from './Projects'
-import Resume from './Resume'
-import ScrollArrow from './ScrollArrow'
-import Skills from './Skills'
-import '../styles/App.css'
-import '../styles/styles.css'
+import AboutMe from "./AboutMe"
+import Contact from "./Contact"
+import Footer from "./Footer"
+import Home from "./Home"
+import Nav from "./Nav"
+import Projects from "./Projects"
+import Resume from "./Resume"
+import ScrollArrow from "./ScrollArrow"
+import Skills from "./Skills"
+// import "../styles/App.css"
+import "../styles/styles.css"
 
 function App() {
   return (
     <>
       <Nav />
       <ScrollArrow />
-      <Route path='/' exact component={Home} />
-      <Route to='/' exact component={AboutMe} />
-      <Route to='/' exact component={Skills} />
-      <Route to='/' exact component={Resume} />
-      <Route to='/' exact component={Projects} />
-      <Route to='/' exact component={Contact} />
+      <Route path="/" exact component={Home} />
+      <Route to="/" exact component={AboutMe} />
+      <Route to="/" exact component={Skills} />
+      <Route to="/" exact component={Resume} />
+      <Route to="/" exact component={Projects} />
+      <Route to="/" exact component={Contact} />
+      <Footer />
     </>
   )
 }
