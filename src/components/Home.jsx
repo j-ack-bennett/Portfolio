550384c5d956646396ab18e0f7023826af158f03
j-ack-bennett <bennettjack44@gmail.com>
2021-05-18 19:05:36 +1200

    added picture and animations

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import { Link } from 'react-scroll'
+import React from "react"
+import AOS from "aos"
+import { Link } from "react-scroll"
 
 import image from "../assets/jack.JPG"
-import '../styles/styles.css'
+import "../styles/styles.css"
 
 const Home = () => {
+  AOS.init()
+
   return (
     <section id="home" className="hero is-link is-fullheight is-fullheight-with-navbar">
       <div className="hero-body">
-        <div className="container" style={{ "marginLeft": "180px" }}>Kia ora! I am
-          <h1 className="title is-1">
-            Jack Bennett
-          </h1>
-          <h2 className="subtitle is-3">
-            Full Stack Software Developer
-          </h2>
+        <div className="container" style={{ "marginLeft": "180px" }} data-aos="fade" data-aos-delay={300} data-aos-duration={1000}>Kia ora! I am
+          <h1 className="title is-1">Jack Bennett</h1>
+          <h2 className="subtitle is-3">Full Stack Software Developer</h2>
+          <h3 className="subtitle is-4 pt-2">Based in Te Whanga-nui-a-Tara, Aotearoa.</h3>
           <div className="pt-6 scroll">
             <Link className="is-size-5" to="about-me" spy={true} smooth={true} duration={1000}>Click to Scroll Below</Link>
           </div>
         </div>
-
+        
         <div className="image-wrapper">
-          <img className="home-image" src={image} alt="Jack" />
+          <img className="home-image" src={image} alt="Jack" data-aos="fade-down-right" data-aos-delay={300} data-aos-duration={1000} />
         </div>
       </div>
     </section>
