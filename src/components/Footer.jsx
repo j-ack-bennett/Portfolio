082a43edcfea3c9d0a7d8942aff7484724463332
j-ack-bennett <bennettjack44@gmail.com>
2021-05-18 19:05:17 +1200

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer class="footer">
+      <div class="section-heading">
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
