768b09b79c573c7705752de78c57945d7a68877e
j-ack-bennett <bennettjack44@gmail.com>
2021-05-18 13:06:34 +1200

    added dunctionality to download cv

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React from 'react'
+import AOS from 'aos'
+import { saveAs } from 'file-saver'
 
 const Resume = () => {
+  AOS.init()
+
+  const saveFile = () => {
+    saveAs(
+      process.env.PUBLIC_URL + "/resources/cv.pdf",
+      "JackBennettCV.pdf"
+    )
+  }
+
   return (
-    <>
-    </>
+    <section id="resume" className="section section-spacing" data-aos="fade-up">
+      <div className="section-heading">
+        <h3 className="title is-2">Resume</h3>
+        <h4 className="subtitle is-5">More about my previous work experience and skills</h4>
+        <button onClick={saveFile} className="button is-link is-medium">
+          <span className="icon">
+            <i className="fas fa-file-alt"></i>
+          </span>
+          <span>Download My Resume</span>
+        </button>
+      </div>
+    </section>
   )
 }
 
