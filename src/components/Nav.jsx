37bb3ee6f9d3bb39fa23e94e2e11cb4f8383aeba
j-ack-bennett <bennettjack44@gmail.com>
2021-05-18 13:05:59 +1200

    added react-scroll links, yet to complete

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,48 +1,47 @@
 import React from 'react'
+import { Link } from 'react-scroll'
 
 const Nav = () => {
   return (
     <nav className="navbar is-link is-fixed-top">
-      <div className="navbar-brand">
-        <div className="navbar-burger burger" data-target="navbarExampleTransparentExample">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-
       <div id="navbarExampleTransparentExample" className="navbar-menu">
         <div className="navbar-end">
-          <a className="navbar-item" href="#about">
+
+          <Link className="navbar-item" to="about-me" spy={true} smooth={true} duration={1000}>
             <span className="icon">
               <i className="fa fa-child"></i>
             </span>
             <span>About Me</span>
-          </a>
-          <a className="navbar-item" href="#services">
+          </Link>
+
+          <Link className="navbar-item" to="skills" spy={true} smooth={true} duration={1000}>
             <span className="icon">
               <i className="fas fa-award"></i>
             </span>
             <span>Skills</span>
-          </a>
-          <a className="navbar-item" href="#resume">
+          </Link>
+
+          <Link className="navbar-item" to="resume" spy={true} smooth={true} duration={1000}>
             <span className="icon">
               <i className="fas fa-file-alt"></i>
             </span>
             <span>Resume</span>
-          </a>
+          </Link>
+
           <a className="navbar-item" href="#portfolio">
             <span className="icon">
               <i className="	fa fa-laptop"></i>
             </span>
             <span>Projects</span>
           </a>
+
           <a className="navbar-item" href="#contact">
             <span className="icon">
               <i className="fas fa-envelope"></i>
             </span>
             <span>Contact</span>
           </a>
+
         </div>
       </div>
     </nav>
