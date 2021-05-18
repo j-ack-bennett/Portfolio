141577493228b6aa393ba342ccf30ec82c368291
j-ack-bennett <bennettjack44@gmail.com>
2021-05-18 13:06:51 +1200

    added arrow to scroll back to top

---
+++ src/components/ScrollArrow.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { Link } from 'react-scroll'
+
+import '../styles/App.css'
+
+const ScrollArrow = () => {
+  const [showScroll, setShowScroll] = useState(false)
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 400){
+      setShowScroll(true)
+    } else if (showScroll && window.pageYOffset <= 400){
+      setShowScroll(false)
+    }
+  }
+
+  window.addEventListener('scroll', checkScrollTop)
+
+  return (
+    <Link 
+      to="home" 
+      spy={true} 
+      smooth={true} 
+      duration={1000} 
+      className="scrollTop" 
+      style={{ height: 40, display: showScroll ? 'flex' : 'none' }}
+    >
+      <i className="fa fa-arrow-circle-up fa-3x"></i>
+    </Link>
+  )
+}
+
+export default ScrollArrow
