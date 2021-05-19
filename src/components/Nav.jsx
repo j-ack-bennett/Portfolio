41da98b57769493d4d439ea27deac3ef8b2f6494
Jack Bennett <bennettjack44@gmail.com>
2021-05-19 16:25:12 +1200

    changed order of resume

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -25,35 +25,35 @@ const Nav = () => {
 
       <div id="navbarMenu" className={`navbar-menu ${isBurgerVisible ? "is-active" : ""}`}>
         <div className="navbar-end">
-          <Link className="navbar-item" to="about-me" spy={true} smooth={true} duration={1000}>
+          <Link className="navbar-item" to="about-me" spy={true} smooth={true} duration={2000}>
             <span className="icon">
               <i className="fa fa-child"></i>
             </span>
             <span>About Me</span>
           </Link>
 
-          <Link className="navbar-item" to="skills" spy={true} smooth={true} duration={1000}>
+          <Link className="navbar-item" to="skills" spy={true} smooth={true} duration={2000}>
             <span className="icon">
               <i className="fas fa-award"></i>
             </span>
             <span>Skills</span>
           </Link>
 
-          <Link className="navbar-item" to="resume" spy={true} smooth={true} duration={1000}>
-            <span className="icon">
-              <i className="fas fa-file-alt"></i>
-            </span>
-            <span>Resume</span>
-          </Link>
-
-          <Link className="navbar-item" to="projects" spy={true} smooth={true} duration={1000}>
+          <Link className="navbar-item" to="projects" spy={true} smooth={true} duration={2000}>
             <span className="icon">
               <i className="fa fa-laptop"></i>
             </span>
             <span>Projects</span>
           </Link>
 
-          <Link className="navbar-item" to="contact" spy={true} smooth={true} duration={1000}>
+          <Link className="navbar-item" to="resume" spy={true} smooth={true} duration={2000}>
+            <span className="icon">
+              <i className="fas fa-file-alt"></i>
+            </span>
+            <span>Resume</span>
+          </Link>
+
+          <Link className="navbar-item" to="contact" spy={true} smooth={true} duration={2000}>
             <span className="icon">
               <i className="fas fa-envelope"></i>
             </span>
