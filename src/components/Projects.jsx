e9b5f78513abe5688ba3a125b0a6cff8050c8035
Jack Bennett <bennettjack44@gmail.com>
2021-05-26 13:21:25 +1200

    removed aos/stylesheet import and added aos options

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,38 +1,35 @@
 import React from "react"
-import AOS from "aos"
 
 import image from "../assets/swopzies.png"
-import "../styles/styles.css"
 
 const Projects = () => {
-  AOS.init()
-
   return (
-    <section className="section section-spacing" id="projects">
+    <section id="projects" className="section section-spacing">
       <div className="container">
         <div className="section-heading">
-          <h3 className="title is-2" data-aos="fade-down">Projects</h3>
-          <div data-aos="fade-up">
-            <p className="subtitle is-5 pt-5">
-              I'm currently focussed on learning to develop iOS applications. As this is a reasonbly new realm of development for me, 
-              I decided to start with learning React Native. Having previous experience with React has allowed me to transition into React Native a lot easier than using a different framework. 
-              Ultimately I would like to build app's with a native framework like Swift, but I am making do with the operating systems I currently have available to me.
-            </p>
-            <p className="subtitle is-5">
-              My aim is to bring my ideas to fruition and build mobile applications that will have positive effects on our community.
-              UI and UX design always comes forefront to me, so as a developer I always incorporate simple functionality and appealing features for the user. Having operated iOS devices
-              for a long time now, I've picked up on small things through various app's that I know could easily be improved for a better user experience.
-            </p>
-            <p className="subtitle is-5">
-              I also have a keen interest in developing small web projects to solidify my knowledge in specific front-end and back-end technologies. 
-              These can be found on my GitHub account linked in my contact section, some of which are still a work in progress.
-            </p>
-          </div>
+          <h3 className="title is-2" data-aos="fade-down" data-aos-duration={1000} data-aos-delay={400}>Projects</h3>
+        </div>
+
+        <div className="has-text-centered" data-aos="fade-up" data-aos-duration={1000} data-aos-delay={400}>
+          <p className="subtitle">
+            I'm currently focussed on learning to develop iOS applications. As this is a reasonbly new realm of development for me, 
+            I decided to start with learning React Native. Having previous experience with React has allowed me to transition into React Native a lot easier than using a different framework. 
+            Ultimately I would like to build app's with a native framework like Swift, but I am making do with the operating systems I currently have available to me.
+          </p>
+          <p className="subtitle">
+            My aim is to bring my ideas to fruition and build mobile applications that will have positive effects on our community.
+            UI and UX design always comes forefront to me, so as a developer I always incorporate simple functionality and appealing features for the user. Having operated iOS devices
+            for a long time now, I've picked up on small things through various app's that I know could easily be improved for a better user experience.
+          </p>
+          <p className="subtitle">
+            I also have a keen interest in developing small web projects to solidify my knowledge in specific front-end and back-end technologies. 
+            These can be found on my GitHub account linked in my contact section, some of which are still a work in progress.
+          </p>
         </div>
 
         <br />
 
-        <div className="columns is-centered" data-aos="fade-up" data-aos-delay={200} data-aos-duration={1000}>
+        <div className="columns is-centered" data-aos="fade-up" data-aos-duration={1000} data-aos-delay={400}>
           <div className="column is-full">
             <div className="box">
               <h4 className="title is-4 has-text-centered">Swopzies</h4>
